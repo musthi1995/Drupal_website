895e1af0dc133e5410983f90285797cb67d60bc2
musthi1995 <darkshadow1812.mm+1@gmail.com>
2024-11-03 17:03:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy to"
   type        = string
-  default     = "ap-southeast-1"  # Example region: Oregon
+  default     = "ap-southeast-1"  # Example region
 }
 
 variable "vpc_cidr" {
